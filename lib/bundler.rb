2b2052a6b0a4661cc185a568e2fbf36130cf0192
David Rodríguez <deivid.rodriguez@riseup.net>
2018-09-25 16:38:09 -0300

    Extract config folder name resolution to a method

--- lib/bundler.rb
+++ lib/bundler.rb
@@ -239,8 +239,6 @@ module Bundler
     end
 
     def app_config_path
-      app_config = ENV["BUNDLE_APP_CONFIG"] || ".bundle"
-
       root.join(app_config)
     end
 
@@ -615,6 +613,10 @@ EOF
       yield
     ensure
       ENV.replace(backup)
+    end
+
+    def app_config
+      ENV["BUNDLE_APP_CONFIG"] || ".bundle"
     end
   end
 end
