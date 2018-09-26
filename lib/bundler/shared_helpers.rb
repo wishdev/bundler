b5e12eb4bd7ad988b530abf1c5e99e7ff9140db2
David Rodríguez <deivid.rodriguez@riseup.net>
2018-09-26 14:26:07 -0300

    Move some logic to helper

--- lib/bundler/shared_helpers.rb
+++ lib/bundler/shared_helpers.rb
@@ -15,8 +15,12 @@ module Bundler
   module SharedHelpers
     def root
       gemfile = find_gemfile
-      raise GemfileNotFound, "Could not locate Gemfile" unless gemfile
-      Pathname.new(gemfile).untaint.expand_path.parent
+      return Pathname.new(gemfile).untaint.expand_path.parent if gemfile
+
+      bundle_dir = default_bundle_dir
+      raise GemfileNotFound, "Could not locate Gemfile or .bundle/ directory" unless bundle_dir
+
+      bundle_dir.parent
     end
 
     def default_gemfile
