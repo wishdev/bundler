7704812c59beb9578c38efb59b13914a80f19567
Samuel Giddins <segiddins@segiddins.me>
2018-11-25 13:22:56 +0000

    [Index] Use MatchPlatform for checking platform matches
    This way, force_ruby_platform is taken into account

--- lib/bundler/index.rb
+++ lib/bundler/index.rb
@@ -195,7 +195,7 @@ module Bundler
           if base # allow all platforms when searching from a lockfile
             dependency.matches_spec?(spec)
           else
-            dependency.matches_spec?(spec) && Gem::Platform.match(spec.platform)
+            dependency.matches_spec?(spec) && MatchPlatform.platforms_match?(spec.platform, Bundler.local_platform)
           end
         end
 
